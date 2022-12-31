35cfce7d746596621c9241014e21b397c277f438
Miguel Gargallo <mgargallo16@gmail.com>
2022-12-31 03:32:01 +0100

    main.rs

---
+++ main.rs
@@ -0,0 +1,13 @@
+// THIS CODE DOES NOTHING, IT IS JUST AN EXAMPLE
+
+extern crate crate1;
+extern crate crate2;
+
+use ...
+
+fn main() {
+    // Statements here are executed when the compiled binary is called
+
+    // Print text to the console
+    println!("Hello World!");
+}
